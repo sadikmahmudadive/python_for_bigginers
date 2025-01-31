38529d95b6c85a8eadab40dec792a643f33fcb65
Md. Sadik Mahmud Adive <siradive137@gmail.com>
2025-01-31 13:18:45 +0600

    updated

---
+++ logical_operetors.py
@@ -0,0 +1,13 @@
+has_high_income = True
+has_good_credit = True
+
+if has_high_income and has_good_credit:
+    print ('Aligible for loan')
+    
+#more logical operators : AND, OR, NOT
+
+has_good_credit1 = True
+has_criminal_report = False
+
+if has_good_credit1 and not has_criminal_report:
+    print ('Aligable for loan 2')
